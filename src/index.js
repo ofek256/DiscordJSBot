8ec2c890b38f925ab4d7ce5b20519e414928caca
ofek256 <ofek256a@gmail.com>
2023-04-20 16:32:26 +0300

    bot now runs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const {Client, IntentsBitField} = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({
     intents: [
@@ -9,5 +10,21 @@ const client = new Client({
     ],
 });
 
-client.login(token);
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is ready`);
+})
 
+client.on('messageCreate',(msg) => {
+    if (msg.author.bot) {
+        return;
+    }
+    if (msg.content === 'hiya') {
+         msg.reply(`${msg.content} abook`);
+         console.log (msg.content);
+        }
+    if (msg.content === 'meow') {
+        msg.reply ('shut the fuck up');
+    }
+} )
+
+client.login(token);
